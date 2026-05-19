9c3d9aaaa3f4c1db0d1acf01f9b00e852e75c67e
Nathan <nathansferreira469@gmail.com>
2026-05-18 23:05:00 -0300

    Pessoas: ajuste models & views

--- academiapp/apps/pessoas/models.py
+++ academiapp/apps/pessoas/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Person(models.Model):
+class Pessoa(models.Model):
     nome = models.CharField('Nome', max_length=50)
     sobrenome = models.CharField('Sobrenome', max_length=100) 
     email = models.CharField('E-mail', max_length=100)
