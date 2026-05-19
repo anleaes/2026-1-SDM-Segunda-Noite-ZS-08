f8eb6879b2a051018fabfcd7ac4e84535bd79e02
Nathan <nathansferreira469@gmail.com>
2026-05-19 19:15:16 -0300

    Alunos: models

--- academiapp/apps/alunos/apps.py
+++ academiapp/apps/alunos/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class AlunosConfig(AppConfig):
     name = 'alunos'
+    verbose_name = 'Alunos'
--- academiapp/apps/alunos/models.py
+++ academiapp/apps/alunos/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from pessoas.models import Pessoa
+from django.utils import timezone
 
 # Create your models here.
+class Alunos(pessoa):
+    genero_opcoes = [
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+        ('O', 'Outro'),
+        ('N/A', 'Prefiro não informar'), 
+    ]
+
+    peso = models.FloatField('Peso', help_text='Preencha em kg (exemplo: 70.5)', blank=True, null=True)
+    altura = models.FloatField('Altura',  help_text='Preencha em metros (exemplo: 1.70)', blank=True, null=True)
+    genero = models.CharField('Gênero', max_length=1, choices=genero_opcoes)
+    objetivo = models.TextField('Objetivo do aluno', blank=True, null=True)
+    data_matricula = models.DateField('Data da matrícula', default=timezone.now)
+
+    class Meta:
+        verbose_name = 'Aluno'
+        verbose_name_plural='Alunos'
+        ordering['id']
+    
+    def __str__(self):
+        return super().__str__()
