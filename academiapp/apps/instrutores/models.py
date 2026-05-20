2c4cb406a09382d90debc3a3ef0b5d06e74d4184
Nathan <nathansferreira469@gmail.com>
2026-05-19 21:27:43 -0300

    Instrutores: models

---
+++ academiapp/apps/instrutores/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from Pessoas.models import Pessoa
+import datetime
+
+# Create your models here.
+class Instrutor(Pessoa):
+    especialidade_opcoes:[
+        ('MUSC', 'Musculação'),
+        ('FUNC', 'Treinamento Funcional'),
+        ('PILA', 'Pilates'),
+        ('CROS', 'Cross Training'),
+        ('DANC', 'Dança / Ritmos'),
+        ('LUTA', 'Artes Marciais'),
+        ('NATA', 'Natação'),
+        ('GERA', 'Ginástica Geral'),
+    ]
+
+    cref = models.CharField('CREF', max_length=12, unique=True, help_text='Exemplo: 000000-G/RS')
+    especialidade = models.CharField('Especialidade', max_length=4, choices=especialidade_opcoes)
+    salario = models.DecimalField('Salário', max_digits=10, decimal_places=2)
+    data_admissao = models.DateField('Data de admissão', default=datetime.date.today)
+
+    class Meta:
+        verbose_name = 'Instrutor'
+        verbose_name_plural= 'Instrutores'
+        ordering = ['-data_admissao']
+
+    def __str__(self):
+        return super().__str__()
