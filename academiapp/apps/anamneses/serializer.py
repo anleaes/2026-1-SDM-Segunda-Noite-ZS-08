948d8a6d7bd947852b99fac96e1a336633561039
Nathan Ferreira <nathansferreira469@gmail.com>
2026-05-22 19:18:32 -0300

    Anamnese: ajuste serializar para ordenar melhor os campos do forms

--- academiapp/apps/anamneses/serializer.py
+++ academiapp/apps/anamneses/serializer.py
@@ -4,4 +4,16 @@ from rest_framework import serializers
 class AnamneseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Anamnese
-        fields = '__all__'
+        fields = [
+            'aluno',
+            'ultima_atualizacao',
+            'alergias',
+            'restricoes_fisicas',
+            'medicamentos_em_uso',
+            'pressao_arterial',
+            'diabetes',
+            'fumante',
+            'problemas_cardiacos', 
+            'cirurgias_recentes',
+            'observacoes'
+        ]
