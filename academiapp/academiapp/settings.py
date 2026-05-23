739d53fdd6c7a644db62e7fbea1e64d504583328
Nathan Ferreira <nathansferreira469@gmail.com>
2026-05-23 15:47:38 -0300

    Refeições: create app + settings e apps

--- academiapp/academiapp/settings.py
+++ academiapp/academiapp/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'planosmensalidade.apps.PlanosMensalidadeConfig',
     'anamneses.apps.AnamnesesConfig',
     'alimentos.apps.AlimentosConfig',
+    'refeicoes.apps.RefeicoesConfig',
 ]
 
 MIDDLEWARE = [
