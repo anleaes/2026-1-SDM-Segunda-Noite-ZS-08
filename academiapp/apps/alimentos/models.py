0b0b00c21222433fb460b1f6fee515b48b5cc475
Nathan Ferreira <nathansferreira469@gmail.com>
2026-05-23 15:12:25 -0300

    Alimentos: models

--- academiapp/apps/alimentos/models.py
+++ academiapp/apps/alimentos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Alimento(models.Model):
+    nome = models.CharField(max_length=100, unique=True, verbose_name="Nome do Alimento")
+    calorias_por_100g = models.DecimalField(max_digits=6, decimal_places=2, verbose_name="Calorias por 100g (kcal)")
+    proteinas_g = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Proteínas (g)")
+    carboidratos_g = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Carboidratos (g)")
+    gorduras_g = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Gorduras (g)")
+    fibras_g = models.DecimalField(max_digits=5, decimal_places=2, default=0.0, verbose_name="Fibras (g)")
+
+    class Meta:
+        verbose_name = "Alimento"
+        verbose_name_plural = "Alimentos"
+        ordering = ['nome']
+
+    def __str__(self):
+        return self.nome
