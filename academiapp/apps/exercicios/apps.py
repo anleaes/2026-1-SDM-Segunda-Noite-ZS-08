d817f282534cb7f62d6532d6be1440d611950f97
aragon <aragonyuri1.as@gmail.com>
2026-05-24 20:54:33 -0300

    Criado o arquivo serializers.py para o app Exercicio

--- academiapp/apps/exercicios/apps.py
+++ academiapp/apps/exercicios/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class ExerciciosConfig(AppConfig):
     name = 'exercicios'
+    verbose_name = 'Exercicios'
--- academiapp/apps/exercicios/models.py
+++ academiapp/apps/exercicios/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+class Exercicio(models.Model):
+    DIFICULDADE_CHOICES = [
+        ('B', 'Iniciante'),
+        ('I', 'Intermediário'),
+        ('A', 'Avançado'),
+    ]
+
+    nome = models.CharField(
+        max_length=100, 
+        verbose_name="Nome do Exercício"
+    )
+    descricao = models.TextField(
+        blank=True, 
+        null=True, 
+        verbose_name="Descrição"
+    )
+    instrucoes = models.TextField(
+        blank=True, 
+        null=True, 
+        verbose_name="Instruções de Execução"
+    )
+    dificuldade = models.CharField(
+        max_length=1, 
+        choices=DIFICULDADE_CHOICES, 
+        default='B', 
+        verbose_name="Grau de Dificuldade"
+    )
+
+    class Meta:
+        verbose_name = "Exercício"
+        verbose_name_plural = "Exercícios"
+        ordering = ['nome']
+
+    def __str__(self):
+        return self.nome
+
