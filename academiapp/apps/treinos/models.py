ace0d773384d66e23905e1b7465353f693c6ef4c
Nathan Ferreira <nathansferreira469@gmail.com>
2026-05-25 18:48:19 -0300

    Treinos: correção models

--- academiapp/apps/treinos/models.py
+++ academiapp/apps/treinos/models.py
@@ -6,8 +6,8 @@ from instrutores.models import Instrutor
 
 # Create your models here.
 class Treino(models.Model):
-    aluno = models.ForeignKey('usuarios.Aluno', on_delete=models.CASCADE, related_name='treinos')
-    instrutor = models.ForeignKey('usuarios.Instrutor', on_delete=models.SET_NULL, null=True, related_name='treinos_criados')
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE, related_name='treinos')
+    instrutor = models.ForeignKey(Instrutor, on_delete=models.SET_NULL, null=True, related_name='treinos_criados')
     
     nome = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
