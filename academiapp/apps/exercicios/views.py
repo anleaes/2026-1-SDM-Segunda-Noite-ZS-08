3e7919552bfa6b33cb07d7ccb6f8e52d4290aa1a
aragon <aragonyuri1.as@gmail.com>
2026-05-24 21:02:08 -0300

    Criado a ExercicioViewSet no views.py para gerenciar as rotas da API

--- academiapp/apps/exercicios/views.py
+++ academiapp/apps/exercicios/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Exercicio
+from rest_framework import viewsets
+from .serializer import ExercicioSerializer
 
 # Create your views here.
+class ExercicoViewSet(viewsets.ModelViewSet):
+    queryset = Exercicio.objects.all()
+    serializer_class = ExercicioSerializer  
