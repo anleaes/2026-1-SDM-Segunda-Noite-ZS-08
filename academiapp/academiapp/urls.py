3e974fea79512bc7287690e6fee652107e5f767e
aragon <aragonyuri1.as@gmail.com>
2026-05-24 21:23:16 -0300

    Conectado as URLs do app exercicios ao arquivo urls.py principal do projeto

--- academiapp/academiapp/urls.py
+++ academiapp/academiapp/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('anamneses/', include('anamneses.urls', namespace='anamneses')),
     path('alimentos/', include('alimentos.urls', namespace='alimentos')),
     path('refeicoes/', include('refeicoes.urls', namespace='refeicoes')),
+    path('exercicios/', include('exercicios.urls', namespace='exercicios')),
 ]
