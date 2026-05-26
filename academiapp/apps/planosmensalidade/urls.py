8df3f2f45919076375820f2e35d0366ddbb17fb5
Nathan <nathansferreira469@gmail.com>
2026-05-26 17:59:21 -0300

    feat(planosmensalidade): cria urls e adiciona router & urlpatterns

---
+++ academiapp/apps/planosmensalidade/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from .import views
+from rest_framework import routers
+
+app_name = 'planosmensalidade'
+
+router = routers.SimpleRouter()
+router.register('', views.PlanosMensalidadeViewSet, basename='planosmensalidade')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
