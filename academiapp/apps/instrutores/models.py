d6ba0a1ce8ae31da124c20c35c60a9c61d656928
Nathan <nathansferreira469@gmail.com>
2026-05-26 19:20:46 -0300

    feat(instrutores): cria atributos no model e migração inicial

--- academiapp/apps/instrutores/models.py
+++ academiapp/apps/instrutores/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from pessoas.models import Pessoa
+import datetime
 
 # Create your models here.
+class Instrutor(Pessoa):
+    especialidade_opcoes = [
+        ('MUSC', 'Musculação'),
+        ('FUNC', 'Treinamento Funcional'),
+        ('PILA', 'Pilates'),
+        ('CROS', 'Cross Training'),
+        ('DANC', 'Dança / Ritmos'),
+        ('LUTA', 'Artes Marciais'),
+        ('NATA', 'Natação'),
+        ('GERA', 'Ginástica Geral'),
+    ]
+
+    cref = models.CharField('CREF', max_length=12, unique=True, help_text='Exemplo: 000000-G/RS')
+    especialidade = models.CharField('Especialidade', max_length=4, choices=especialidade_opcoes)
+    salario = models.DecimalField('Salário', max_digits=10, decimal_places=2)
+    data_admissao = models.DateField('Data de admissão', default=datetime.date.today)   
