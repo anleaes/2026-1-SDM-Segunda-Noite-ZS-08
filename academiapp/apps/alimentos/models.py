68d55597be698939f1a7ce9ccefd4fb92ac79c31
Cristopher Martins Baroni <cmbaroni97@gmail.com>
2026-05-27 04:16:19 -0300

    feat: adiciona classe Meta e metodo str na classe Alimento

--- academiapp/apps/alimentos/models.py
+++ academiapp/apps/alimentos/models.py
@@ -8,3 +8,10 @@ class Alimento(models.Model):
     gorduras_g = models.DecimalField(max_digits=6, decimal_places=2)
     fibras_g = models.DecimalField(max_digits=6, decimal_places=2)
 
+    class Meta:
+        db_table = 'alimentos'
+        verbose_name = 'Alimento'
+        verbose_name_plural = 'Alimentos'
+
+    def __str__(self):
+        return self.nome
