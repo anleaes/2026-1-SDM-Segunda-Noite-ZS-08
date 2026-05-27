3136ba93b4f56dcdf4930465b12159361ea6e53c
Cristopher Martins Baroni <cmbaroni97@gmail.com>
2026-05-27 05:25:24 -0300

    feat: configura roteador e rotas de refeicoes

--- academiapp/apps/refeicoes/urls.py
+++ academiapp/apps/refeicoes/urls.py
@@ -3,3 +3,10 @@ from rest_framework import routers
 from . import views
 
 app_name = 'refeicoes'
+
+router = routers.SimpleRouter()
+router.register('', views.RefeicaoViewSet, basename='refeicoes')
+
+urlpatterns = [
+    path('', include(router.urls))
+]
