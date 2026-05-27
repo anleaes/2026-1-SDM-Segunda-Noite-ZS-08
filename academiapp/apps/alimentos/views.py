d37a62d446045b79fa8427fa761fda7ca7278ea8
Cristopher Martins Baroni <cmbaroni97@gmail.com>
2026-05-27 04:55:18 -0300

    feat: configura viewset para o modelo Alimento

--- academiapp/apps/alimentos/views.py
+++ academiapp/apps/alimentos/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Alimento
 from .serializer import AlimentoSerializer
+
+class AlimentoViewSet(viewsets.ModelViewSet):
+    queryset = Alimento.objects.all()
+    serializer_class = AlimentoSerializer
