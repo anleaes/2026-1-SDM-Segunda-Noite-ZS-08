faa1c476b783d8f0fc253c37f6d321ef47431b79
Cristopher Martins Baroni <cmbaroni97@gmail.com>
2026-05-27 20:51:29 -0300

    feat(treinos): adiciona atributos ao model

--- academiapp/apps/treinos/models.py
+++ academiapp/apps/treinos/models.py
@@ -6,4 +6,14 @@ from instrutores.models import Instrutor
 
 # Create your models here.
 class Treino(models.Model):
-    pass
+
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE, related_name='treinos')
+    instrutor = models.ForeignKey(Instrutor, on_delete=models.SET_NULL, null=True, related_name='treinos_criados')
+    
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_criacao = models.DateField(auto_now_add=True)
+    duracao_minutos = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.nome} - {self.aluno.nome}"
