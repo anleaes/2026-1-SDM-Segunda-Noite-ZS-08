99adae5a12880f088d469fcb5f0fd2c5d8e231e1
Cristopher Martins Baroni <cmbaroni97@gmail.com>
2026-05-27 17:02:39 -0300

    feat(planosalimentares): cria atributos do model e migracao inicial

--- academiapp/apps/planosalimentares/models.py
+++ academiapp/apps/planosalimentares/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from alunos.models import Aluno
 
 # Create your models here.
+class PlanoAlimentar(models.Model):
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE, related_name='planos_alimentares', verbose_name='Aluno')
+    titulo = models.CharField('Título', max_length=100)
+    descricao = models.TextField('Descrição', blank=True, null=True)
+    objetivo = models.CharField('Objetivo', max_length=150)
+    data_inicio = models.DateField('Data de Início')
+    data_fim = models.DateField('Data de Fim',blank=True, null=True)
+    calorias_diarias = models.IntegerField('Calorias Diárias')
