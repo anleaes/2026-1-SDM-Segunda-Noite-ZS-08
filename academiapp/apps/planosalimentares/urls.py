a0e7d094a54d50843fa711cba85dbd6e0334a91e
Cristopher Martins Baroni <cmbaroni97@gmail.com>
2026-05-27 05:14:35 -0300

    feat: configura roteador e rotas de planos alimentares

--- academiapp/apps/planosalimentares/urls.py
+++ academiapp/apps/planosalimentares/urls.py
@@ -3,3 +3,10 @@ from rest_framework import routers
 from . import views
 
 app_name = 'planosalimentares'
+
+router = routers.SimpleRouter()
+router.register('', views.PlanoAlimentarViewSet, basename='planosalimentares')
+
+urlpatterns = [
+    path('', include(router.urls))
+]
