c6d528be187ec30f81027f2902efcef544ca6a85
Cristopher Martins Baroni <cmbaroni97@gmail.com>
2026-05-27 17:48:56 -0300

    feat(refeicoes): cria estrutura inicial do model

--- academiapp/apps/refeicoes/models.py
+++ academiapp/apps/refeicoes/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from alimentos.models import Alimento
+from planosalimentares.models import PlanoAlimentar
 
 # Create your models here.
+class Refeicao(models.Model):
+    pass
