ef695643896f5d5537b67465a5906cc42a70eb9f
Cristopher Martins Baroni <cmbaroni97@gmail.com>
2026-05-27 20:43:07 -0300

    Pequena alteração na nomenclatura da linha 4, para evitar erros no código

--- academiapp/apps/exercicios/views.py
+++ academiapp/apps/exercicios/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Exercicio
 from rest_framework import viewsets
-from .serializer import ExercicioSerializer
+from .serializers import ExercicioSerializer
 
 # Create your views here.
 class ExercicioViewSet(viewsets.ModelViewSet):
