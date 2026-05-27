f10a7081b569a78e52265d50b3f275c497607612
Cristopher Martins Baroni <cmbaroni97@gmail.com>
2026-05-27 05:22:48 -0300

    feat: adiciona classe RefeicaoSerializer

--- academiapp/apps/refeicoes/serializer.py
+++ academiapp/apps/refeicoes/serializer.py
@@ -1,3 +1,7 @@
 from rest_framework import serializers
 from .models import Refeicao
 
+class RefeicaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Refeicao
+        fields = '__all__'
