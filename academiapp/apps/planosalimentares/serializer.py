a58474f1bbd05f53a5b5c87a687d840085e3baa5
Cristopher Martins Baroni <cmbaroni97@gmail.com>
2026-05-27 05:11:30 -0300

    feat: adiciona classe PlanoAlimentarSerializer

--- academiapp/apps/planosalimentares/serializer.py
+++ academiapp/apps/planosalimentares/serializer.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from .models import PlanoAlimentar
+
+class PlanoAlimentarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PlanoAlimentar
+        fields = '__all__'
