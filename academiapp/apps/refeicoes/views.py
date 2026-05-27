2146f022aac2eb9de21ef9846f70f5b89695602e
Cristopher Martins Baroni <cmbaroni97@gmail.com>
2026-05-27 17:59:26 -0300

    feat(refeicoes): cria estrutura inicial da view

--- academiapp/apps/refeicoes/views.py
+++ academiapp/apps/refeicoes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Refeicao
+from rest_framework import viewsets
+from .serializer import RefeicaoSerializer
 
 # Create your views here.
+class RefeicaoViewSet(viewsets.ModelViewSet):
+    pass
