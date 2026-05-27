3c19ad07347e715c1e2b30e4cec4ac7e4d9af9ac
Cristopher Martins Baroni <cmbaroni97@gmail.com>
2026-05-27 17:37:26 -0300

    feat(planosalimentares): cria estrutura inicial da view

--- academiapp/apps/planosalimentares/views.py
+++ academiapp/apps/planosalimentares/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import PlanoAlimentar
+from rest_framework import viewsets
+from .serializer import PlanoAlimentarSerializer
 
-# Create your views here.
+
+class PlanoAlimentarViewSet(viewsets.ModelViewSet):
+    pass
