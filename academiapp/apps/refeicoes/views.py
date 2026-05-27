524c6665a194f6407ae55361bb4d4ccdd5252df0
Cristopher Martins Baroni <cmbaroni97@gmail.com>
2026-05-27 05:24:02 -0300

    feat: adiciona viewset de refeicoes

--- academiapp/apps/refeicoes/views.py
+++ academiapp/apps/refeicoes/views.py
@@ -2,3 +2,6 @@ from rest_framework import viewsets
 from .models import Refeicao
 from .serializer import RefeicaoSerializer
 
+class RefeicaoViewSet(viewsets.ModelViewSet):
+    queryset = Refeicao.objects.all()
+    serializer_class = RefeicaoSerializer
