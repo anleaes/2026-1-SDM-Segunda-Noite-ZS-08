7df8e2db34b7fe96137933b2f43548233dae1b8a
Cristopher Martins Baroni <cmbaroni97@gmail.com>
2026-05-27 17:27:41 -0300

    feat(planosalimentares): adiciona metadados e fields

--- academiapp/apps/planosalimentares/serializers.py
+++ academiapp/apps/planosalimentares/serializers.py
@@ -2,4 +2,7 @@ from .models import PlanoAlimentar
 from rest_framework import serializers
 
 class PlanoAlimentarSerializer(serializers.ModelSerializer):
-    pass
+    
+    class Meta:
+        model = PlanoAlimentar
+        fields = '__all__'
