ce10851392cd32f9dd54fe8646871e8016d683e6
Cristopher Martins Baroni <cmbaroni97@gmail.com>
2026-05-27 05:12:38 -0300

    feat: adiciona viewset de planos alimentares

--- academiapp/apps/planosalimentares/views.py
+++ academiapp/apps/planosalimentares/views.py
@@ -1,3 +1,7 @@
 from rest_framework import viewsets
 from .models import PlanoAlimentar
 from .serializer import PlanoAlimentarSerializer
+
+class PlanoAlimentarViewSet(viewsets.ModelViewSet):
+    queryset = PlanoAlimentar.objects.all()
+    serializer_class = PlanoAlimentarSerializer
