30d909cb0c4045eb9743ca6883d8ca934b7e40d2
Cristopher Martins Baroni <cmbaroni97@gmail.com>
2026-05-27 21:55:23 -0300

    feat(treinos): cria estrutura inicial da view

--- academiapp/apps/treinos/views.py
+++ academiapp/apps/treinos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Treino
+from rest_framework import viewsets
+from .serializers import TreinoSerializer
 
 # Create your views here.
+class TreinoViewSet(viewsets.ModelViewSet):
+    pass
