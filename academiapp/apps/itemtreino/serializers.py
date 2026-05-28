d3e5c910ebda17ed9a45815256a21462853ffae8
Cristopher Martins Baroni <cmbaroni97@gmail.com>
2026-05-27 21:13:06 -0300

    feat(itemtreino): adiciona metadados e fields

--- academiapp/apps/itemtreino/serializers.py
+++ academiapp/apps/itemtreino/serializers.py
@@ -2,4 +2,7 @@ from .models import ItemTreino
 from rest_framework import serializers
     
 class ItemTreinoSerializer(serializers.ModelSerializer):
-    pass
+
+    class Meta:
+        model = ItemTreino
+        fields = '__all__'
