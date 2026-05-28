328f154ecbc5d2a3c9ce9096511b29591608bc84
Cristopher Martins Baroni <cmbaroni97@gmail.com>
2026-05-27 22:24:33 -0300

    feat(itemtreino): regista rotas no router e vincula projeto

--- academiapp/apps/itemtreino/urls.py
+++ academiapp/apps/itemtreino/urls.py
@@ -5,3 +5,8 @@ from rest_framework import routers
 app_name = 'itemtreino'
 
 router = routers.SimpleRouter()
+router.register('', views.ItemTreinoViewSet, basename='itemTreino')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
