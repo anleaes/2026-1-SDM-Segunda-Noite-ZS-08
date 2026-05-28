104c3cb63df2813c0447358d6557915ab34a7748
Cristopher Martins Baroni <cmbaroni97@gmail.com>
2026-05-27 22:11:47 -0300

    feat(itemtreino): cria estrutura base da app

--- academiapp/academiapp/settings.py
+++ academiapp/academiapp/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'refeicoes.apps.RefeicoesConfig',
     'exercicios.apps.ExerciciosConfig',
     'treinos.apps.TreinosConfig',
+    'itemtreino.apps.ItemtreinoConfig',
 ]
 
 MIDDLEWARE = [
