2d31540c0d2d5b5f7305bcc9950fc1be2b1ff3da
Cristopher Martins Baroni <cmbaroni97@gmail.com>
2026-05-27 22:13:40 -0300

    feat(itemtreino): adiciona atributos ao model

--- academiapp/apps/itemtreino/models.py
+++ academiapp/apps/itemtreino/models.py
@@ -6,4 +6,14 @@ from treinos.models import Treino
 
 # Create your models here.
 class ItemTreino(models.Model):
-    pass
+    treino = models.ForeignKey(Treino, on_delete=models.CASCADE, related_name='itens')
+    exercicio = models.ForeignKey(Exercicio, on_delete=models.CASCADE, related_name='itens_treino')
+    
+    series = models.IntegerField()
+    repeticoes = models.IntegerField()
+    carga_kg = models.DecimalField(max_digits=5, decimal_places=2)
+    intervalo_segundos = models.IntegerField()
+    observacoes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.series}x{self.repeticoes} de {self.exercicio.nome} no {self.treino.nome}"
