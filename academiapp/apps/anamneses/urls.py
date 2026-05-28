175e31d7591941d09c6dc9d2df2e47f7ef845585
Nathan <nathansferreira469@gmail.com>
2026-05-28 19:39:44 -0300

    feat(anamnese): adiciona paths do crud

--- academiapp/apps/anamneses/urls.py
+++ academiapp/apps/anamneses/urls.py
@@ -8,5 +8,10 @@ router = routers.SimpleRouter()
 router.register('', views.AnamneseViewSet, basename='anamnese')
 
 urlpatterns = [
+    path('listar/', views.list_anamneses, name='list_anamneses'),
+    path('adicionar/', views.add_anamnese, name='add_anamnese'),
+    path('editar/<int:id_anamnese>/', views.edit_anamnese, name='edit_anamnese'),
+    path('excluir/<int:id_anamnese>/', views.delete_anamnese, name='delete_anamnese'),
+
     path('', include(router.urls))
 ]
