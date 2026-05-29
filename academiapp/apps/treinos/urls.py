e677808a7b9ed4b2aab03494a060a9815d63c49f
Nathan <nathansferreira469@gmail.com>
2026-05-29 12:55:54 -0300

    adição das urls dos novos métodos (montagem de treino)

--- academiapp/apps/treinos/urls.py
+++ academiapp/apps/treinos/urls.py
@@ -9,8 +9,13 @@ router.register('', views.TreinoViewSet, basename='treinos')
 
 urlpatterns = [
     path('listar/', views.list_treinos, name='list_treinos'),
-    path('adicionar/', views.add_treino, name='add_treino'),
-    path('editar/<int:id_treino>/', views.edit_treino, name='edit_treino'),
+    path('exercicios/', views.list_exercicios_add, name='list_exercicios_add'),
+    path('montar/', views.montar_treino, name='montar_treino'),
+    path('montar/adicionar/<int:id_exercicio>/', views.add_exercicio_treino, name='add_exercicio_treino'),
+    path('montar/editar/<int:id_exercicio>/', views.edit_exercicio_treino, name='edit_exercicio_treino'),
+    path('montar/excluir/<int:id_exercicio>/', views.delete_exercicio_treino, name='delete_exercicio_treino'),
+    path('finalizar/', views.finalizar_treino, name='finalizar_treino'),
+    path('visualizar/<int:id_treino>/', views.view_treino, name='view_treino'),
     path('excluir/<int:id_treino>/', views.delete_treino, name='delete_treino'),
     
     path('', include(router.urls) )
