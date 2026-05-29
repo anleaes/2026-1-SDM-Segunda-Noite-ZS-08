06ed08164aed0780c4deab0aa86d16fa790b5121
Nathan <nathansferreira469@gmail.com>
2026-05-29 12:31:39 -0300

    feat(exercicios): adição das urls do método CRUD

--- academiapp/apps/exercicios/urls.py
+++ academiapp/apps/exercicios/urls.py
@@ -8,5 +8,10 @@ router = routers.SimpleRouter()
 router.register('', views.ExercicioViewSet, basename='exercicios')
 
 urlpatterns = [
+    path('listar/', views.list_exercicios, name='list_exercicios'),
+    path('adicionar/', views.add_exercicio, name='add_exercicio'),
+    path('editar/<int:id_exercicio>/', views.edit_exercicio, name='edit_exercicio'),
+    path('excluir/<int:id_exercicio>/', views.delete_exercicio, name='delete_exercicio'),
+
     path('', include(router.urls) )
 ]
