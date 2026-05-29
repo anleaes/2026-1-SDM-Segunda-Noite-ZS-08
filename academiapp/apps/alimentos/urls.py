236fc8a1c58e156af4211508d8def596e41d01a9
aragon <aragonyuri1.as@gmail.com>
2026-05-28 23:52:27 -0300

    declaradas as urls especificas do App alimentos

--- academiapp/apps/alimentos/urls.py
+++ academiapp/apps/alimentos/urls.py
@@ -8,5 +8,12 @@ router = routers.SimpleRouter()
 router.register('', views.AlimentoViewSet, basename='Alimentos')
 
 urlpatterns = [
+    path('listar/', views.list_alimentos, name='list_alimentos'),
+    path('adicionar/', views.add_alimento, name='add_alimento'),
+    path('editar/<int:id_alimento>/', views.edit_alimento, name='edit_alimento'),
+    path('excluir/<int:id_alimento>/', views.delete_alimento, name='delete_alimento'),
+]
+
+urlpatterns = [
     path('', include(router.urls) )
 ]
