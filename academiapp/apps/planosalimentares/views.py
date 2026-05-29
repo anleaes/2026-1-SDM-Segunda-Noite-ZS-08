d2cbf024425edaf32f4423909fef13840b337c11
aragon <aragonyuri1.as@gmail.com>
2026-05-29 00:55:37 -0300

    criacao das views

--- academiapp/apps/planosalimentares/views.py
+++ academiapp/apps/planosalimentares/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import PlanoAlimentar
 from rest_framework import viewsets
 from .serializers import PlanoAlimentarSerializer
+from planosalimentares.models import PlanoAlimentar
+from itemtreino.models import Treino
+from exercicios.models import Exercicio
+from refeicoes.models import Refeicao
+from employees.models import Employee
 
 
 class PlanoAlimentarViewSet(viewsets.ModelViewSet):
