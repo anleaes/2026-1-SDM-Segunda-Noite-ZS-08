3094264a0b560e34ab1563faa3ea5f87560417d3
Nathan <nathansferreira469@gmail.com>
2026-05-29 12:11:19 -0300

    feat(treinos): adição dos metodos no views

--- academiapp/apps/treinos/views.py
+++ academiapp/apps/treinos/views.py
@@ -1,10 +1,51 @@
-from django.shortcuts import render
-from .models import Treino
+from django.shortcuts import render, get_object_or_404, redirect
 from rest_framework import viewsets
-from .serializers import TreinoSerializer
+from .models import Treino
+from .serializers import TreinoSerializer 
+from .forms import TreinoForm
 
-# Create your views here.
 class TreinoViewSet(viewsets.ModelViewSet):
-
     queryset = Treino.objects.all()
-    serializer_class = TreinoSerializer 
+    serializer_class = TreinoSerializer
+
+def add_treino(request):
+    template_name = 'treinos/add_treino.html'
+    context = {}
+    if request.method == 'POST':
+        form = TreinoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('treinos:list_treinos')
+    else:
+        form = TreinoForm()
+    context['form'] = form
+    return render(request, template_name, context)
+
+def list_treinos(request):
+    template_name = 'treinos/list_treinos.html'
+    treinos = Treino.objects.all()
+    context = {
+        'treinos': treinos,
+    }
+    return render(request, template_name, context)
+
+def edit_treino(request, id_treino):
+    template_name = 'treinos/add_treino.html'
+    context = {}
+    treino = get_object_or_404(Treino, id=id_treino)
+    
+    if request.method == 'POST':
+        form = TreinoForm(request.POST, instance=treino)
+        if form.is_valid():
+            form.save()
+            return redirect('treinos:list_treinos')
+    else:
+        form = TreinoForm(instance=treino)
+        
+    context['form'] = form
+    return render(request, template_name, context)
+
+def delete_treino(request, id_treino):
+    treino = get_object_or_404(Treino, id=id_treino)
+    treino.delete()
+    return redirect('treinos:list_treinos')
