031774b3f3616f73d12dfd4bac7defc1c8b83476
Nathan <nathansferreira469@gmail.com>
2026-05-29 10:41:06 -0300

    correcao(alimentos): correção views

--- academiapp/apps/alimentos/views.py
+++ academiapp/apps/alimentos/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .forms import AlimentoForm
-from .models import Alimento
 from rest_framework import viewsets
-from .serializer import AlimentoSerializers
+from .models import Alimento
+from .serializers import AlimentoSerializer
+from .forms import AlimentoForm
 
 class AlimentoViewSet(viewsets.ModelViewSet):
     queryset = Alimento.objects.all()
-    serializer_class = AlimentoSerializers
-
+    serializer_class = AlimentoSerializer
 
 def add_alimento(request):
     template_name = 'alimentos/add_alimento.html'
@@ -15,9 +14,7 @@ def add_alimento(request):
     if request.method == 'POST':
         form = AlimentoForm(request.POST)
         if form.is_valid():
-            f = form.save(commit=False)
-            f.save()
-            form.save_m2m()
+            form.save()
             return redirect('alimentos:list_alimentos')
     form = AlimentoForm()
     context['form'] = form
@@ -25,22 +22,25 @@ def add_alimento(request):
 
 def list_alimentos(request):
     template_name = 'alimentos/list_alimentos.html'
-    alimentos = Alimento.objects.filter()
+    alimentos = Alimento.objects.all()
     context = {
-        'alimentos': alimentos
+        'alimentos': alimentos,
     }
     return render(request, template_name, context)
 
 def edit_alimento(request, id_alimento):
     template_name = 'alimentos/add_alimento.html'
-    context ={}
+    context = {}
     alimento = get_object_or_404(Alimento, id=id_alimento)
+    
     if request.method == 'POST':
         form = AlimentoForm(request.POST, instance=alimento)
         if form.is_valid():
             form.save()
             return redirect('alimentos:list_alimentos')
-    form = AlimentoForm(instance=alimento)
+    else:
+        form = AlimentoForm(instance=alimento)
+        
     context['form'] = form
     return render(request, template_name, context)
 
